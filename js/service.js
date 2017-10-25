374204c37dfb7f89913e95d6d34dda431a2b940c
Mathilde Barbier <mathilde206@gmail.com>
2017-10-25 15:58:23 +0200

    Add relevant flat information

--- js/service.js
+++ js/service.js
@@ -12,16 +12,23 @@ angular.module('ListingModule', [])
     		var  listingsToShow = [];
 			var filteredListingsArr = listingsArr.filter(x => x["resultlist.realEstate"].address.quarter === area);
 
-			filteredListingsArr.forEach(function(listing) {
+			filteredListingsArr.forEach(function(listing, position) {
 			
 			//Create a listing object with information to use
 			var flat = {};
 
-			//get the coordonates
+			//get the coordonates (to add ---> what happens if no coordinates)
+
 			flat.lat = listing["resultlist.realEstate"].address.wgs84Coordinate.latitude;
 			flat.long = listing["resultlist.realEstate"].address.wgs84Coordinate.longitude;
 
 			//get the data for the list
+			flat.link = listing["@xlink.href"];
+			flat.title = listing["resultlist.realEstate"].title;
+			flat.warmRent = listing["resultlist.realEstate"].calculatedPrice.value;
+			flat.livingspace = listing["resultlist.realEstate"].livingSpace;
+			flat.rooms = listing["resultlist.realEstate"].numberOfRooms;
+			flat.nb = position +1;
 
 			listingsToShow.push(flat);
 
